b7038eddf473d2ad7111ba37f035644fe1c228dc
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-02-05 16:38:58 +0300

    Create bem-block.js

---
+++ bem-block.js
@@ -0,0 +1,13 @@
+(function(BEM, $) {
+    'use strict';
+
+    BEM.DOM.decl('$NAME', {
+        onSetMod: {
+            js: {
+                inited: function() {
+                    
+                }
+            }
+        }
+    }, {});
+})(BEM, jQuery);
